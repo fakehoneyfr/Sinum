5e318eb6dda83ccdce6ae8a0d2d6066914251e2c
fakehoneyfr <fakehoney123456@gmail.com>
2025-12-08 19:23:51 -0500

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://147.185.221.229:27165"
+#define API_URL @"http://184.105.7.244:8080"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
